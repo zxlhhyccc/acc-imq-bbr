0dcc6efec029db36cefa3c82cd2f96860aa4bcd4
zxlhhyccc <zxlhhy@gmail.com>
2020-03-21 15:29:02 +0800

    aaa

--- master/package/network/services/dnsmasq/files/50-dnsmasq-migrate-resolv-conf-auto.sh
+++ master/package/network/services/dnsmasq/files/50-dnsmasq-migrate-resolv-conf-auto.sh
@@ -2,7 +2,6 @@
 
 [ "$(uci get dhcp.@dnsmasq[0].resolvfile)" = "/tmp/resolv.conf.auto" ] && {
 	uci set dhcp.@dnsmasq[0].resolvfile="/tmp/resolv.conf.d/resolv.conf.auto"
-rm -f /tmp/resolv.conf.auto && ln -s /tmp/resolv.conf.d/resolv.conf.auto /tmp/resolv.conf.auto
 	uci commit dhcp
 }
 
