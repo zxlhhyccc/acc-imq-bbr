8d69e9a72c348dbf2632d27bbfed3d01fed70011
zxlhhyccc <zxlhhyccc@gmail.com>
2022-05-04 02:31:00 +0800

    mac80211: add ath11k support

--- master/qca-5.10-5.15/package/kernel/mac80211/files/lib/wifi/mac80211.sh
+++ master/qca-5.10-5.15/package/kernel/mac80211/files/lib/wifi/mac80211.sh
@@ -182,7 +182,7 @@ detect_mac80211() {
 			set wireless.default_radio${devidx}.network=lan
 			set wireless.default_radio${devidx}.mode=ap
 			set wireless.default_radio${devidx}.ssid=OpenWrt
-			set wireless.default_radio${devidx}.encryption=sae-mixed
+			set wireless.default_radio${devidx}.encryption=psk2
 			set wireless.default_radio${devidx}.key=1234567890
 EOF
 		uci -q commit wireless
