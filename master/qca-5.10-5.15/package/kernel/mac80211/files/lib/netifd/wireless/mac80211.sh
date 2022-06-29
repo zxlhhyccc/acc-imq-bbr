e884af2c1a04426db179a7a209c8dad57cb83169
zxlhhyccc <zxlhhyccc@gmail.com>
2022-06-30 00:58:02 +0800

    mac80211: refresh patch

--- master/qca-5.10-5.15/package/kernel/mac80211/files/lib/netifd/wireless/mac80211.sh
+++ master/qca-5.10-5.15/package/kernel/mac80211/files/lib/netifd/wireless/mac80211.sh
@@ -427,20 +427,13 @@ mac80211_hostapd_setup_base() {
 		he_mac_cap=${he_mac_cap:2}
 
 		append base_cfg "ieee80211ax=1" "$N"
-
-		if [ -n "$he_bss_color" ]; then
-			append base_cfg "he_bss_color=$he_bss_color" "$N"
-		else
-			he_bss_color=$(head -1 /dev/urandom | tr -dc '0-9' | head -c2)
-			he_bss_color=$(($he_bss_color % 63))
-			he_bss_color=$(($he_bss_color + 1))
-			append base_cfg "he_bss_color=$he_bss_color" "$N"
-		fi
-
 		[ "$hwmode" = "a" ] && {
 			append base_cfg "he_oper_chwidth=$vht_oper_chwidth" "$N"
 			append base_cfg "he_oper_centr_freq_seg0_idx=$vht_center_seg0" "$N"
 		}
+
+		set_default he_bss_color 128
+		append base_cfg "he_bss_color=$he_bss_color" "$N"
 
 		mac80211_add_he_capabilities \
 			he_su_beamformer:${he_phy_cap:6:2}:0x80:$he_su_beamformer \
