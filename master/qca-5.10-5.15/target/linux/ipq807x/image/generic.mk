93a4eefe2aa7464b5142ce8bd8672a47f82fed34
zxlhhyccc <zxlhhyccc@gmail.com>
2023-04-29 15:08:54 +0800

    ipq807x: kernel 5.15.108

--- master/qca-5.10-5.15/target/linux/ipq807x/image/generic.mk
+++ master/qca-5.10-5.15/target/linux/ipq807x/image/generic.mk
@@ -17,22 +17,24 @@ define Device/UbiFit
 	IMAGE/sysupgrade.bin := sysupgrade-tar | append-metadata
 endef
 
-define Device/IfnameMigration
-	DEVICE_COMPAT_VERSION := 1.1
-	DEVICE_COMPAT_MESSAGE := Network interface names have changed
+define Device/buffalo_wxr-5950ax12
+	$(call Device/FitImage)
+	$(call Device/UbiFit)
+	DEVICE_VENDOR := Buffalo
+	DEVICE_MODEL := WXR-5950AX12
+	BLOCKSIZE := 128k
+	PAGESIZE := 2048
+	DEVICE_DTS_CONFIG := config@hk01
+	SOC := ipq8074
+	IMAGES := sysupgrade.bin
+	IMAGE/sysupgrade.bin := sysupgrade-tar | append-metadata
+	DEVICE_PACKAGES := ipq-wifi-buffalo_wxr-5950ax12
 endef
-
-define Device/partition-layout-migration
-  DEVICE_COMPAT_VERSION := 2.0
-  DEVICE_COMPAT_MESSAGE := *** Partition layout has changed from earlier \
-	versions. You need to reinstall the firmware from UART or a migration \
-	initramfs image. Settings will be lost. ***
-endef
+TARGET_DEVICES += buffalo_wxr-5950ax12
 
 define Device/dynalink_dl-wrx36
 	$(call Device/FitImage)
 	$(call Device/UbiFit)
-	$(call Device/IfnameMigration)
 	DEVICE_VENDOR := Dynalink
 	DEVICE_MODEL := DL-WRX36
 	BLOCKSIZE := 128k
@@ -46,7 +48,6 @@ TARGET_DEVICES += dynalink_dl-wrx36
 define Device/edgecore_eap102
 	$(call Device/FitImage)
 	$(call Device/UbiFit)
-	$(call Device/IfnameMigration)
 	DEVICE_VENDOR := Edgecore
 	DEVICE_MODEL := EAP102
 	BLOCKSIZE := 128k
@@ -61,7 +62,6 @@ TARGET_DEVICES += edgecore_eap102
 define Device/edimax_cax1800
 	$(call Device/FitImage)
 	$(call Device/UbiFit)
-	$(call Device/IfnameMigration)
 	DEVICE_VENDOR := Edimax
 	DEVICE_MODEL := CAX1800
 	BLOCKSIZE := 128k
@@ -72,9 +72,25 @@ define Device/edimax_cax1800
 endef
 TARGET_DEVICES += edimax_cax1800
 
+define Device/netgear_wax218
+	$(call Device/FitImage)
+	$(call Device/UbiFit)
+	ARTIFACTS := web-ui-factory.fit
+	DEVICE_VENDOR := Netgear
+	DEVICE_MODEL := WAX218
+	DEVICE_DTS_CONFIG := config@hk07
+	BLOCKSIZE := 128k
+	PAGESIZE := 2048
+	SOC := ipq8072
+	ARTIFACT/web-ui-factory.fit := append-image initramfs-uImage.itb | \
+		ubinize-kernel | qsdk-ipq-factory-nand
+	DEVICE_PACKAGES := kmod-spi-gpio kmod-spi-bitbang kmod-gpio-nxp-74hc164 \
+		ipq-wifi-netgear_wax218
+endef
+TARGET_DEVICES += netgear_wax218
+
 define Device/qnap_301w
 	$(call Device/FitImage)
-	$(call Device/IfnameMigration)
 	DEVICE_VENDOR := QNAP
 	DEVICE_MODEL := 301w
 	DEVICE_DTS_CONFIG := config@hk01
@@ -99,7 +115,6 @@ TARGET_DEVICES += redmi_ax6
 define Device/xiaomi_ax3600
 	$(call Device/FitImage)
 	$(call Device/UbiFit)
-	$(call Device/IfnameMigration)
 	DEVICE_VENDOR := Xiaomi
 	DEVICE_MODEL := AX3600
 	BLOCKSIZE := 128k
@@ -119,7 +134,6 @@ TARGET_DEVICES += xiaomi_ax3600
 define Device/xiaomi_ax9000
 	$(call Device/FitImage)
 	$(call Device/UbiFit)
-	$(call Device/IfnameMigration)
 	DEVICE_VENDOR := Xiaomi
 	DEVICE_MODEL := AX9000
 	BLOCKSIZE := 128k
@@ -139,7 +153,6 @@ TARGET_DEVICES += xiaomi_ax9000
 define Device/zte_mf269
     $(call Device/FitImage)
     $(call Device/UbiFit)
-    $(call Device/IfnameMigration)
     DEVICE_VENDOR := ZTE
     DEVICE_MODEL := MF269
     BLOCKSIZE := 128k
@@ -153,7 +166,6 @@ TARGET_DEVICES += zte_mf269
 define Device/tplink_xtr10890
 	$(call Device/FitImage)
 	$(call Device/UbiFit)
-	$(call Device/IfnameMigration)
 	DEVICE_VENDOR := TPLINK
 	DEVICE_MODEL := XTR10890
 	BLOCKSIZE := 128k
@@ -163,3 +175,21 @@ define Device/tplink_xtr10890
 	DEVICE_PACKAGES := ipq-wifi-tplink_xtr10890 uboot-envtools
 endef
 TARGET_DEVICES += tplink_xtr10890
+
+define Device/zyxel_nbg7815
+	$(call Device/FitImage)
+	DEVICE_VENDOR := ZYXEL
+	DEVICE_MODEL := NBG7815
+	DEVICE_TITLE := Zyxel Armor G5 (NBG7815)
+	DEVICE_DTS_CONFIG := config@nbg7815
+	BLOCKSIZE := 128k
+	PAGESIZE := 2048
+	SOC := ipq8074
+	IMAGES += factory.bin sysupgrade.bin
+	IMAGE/factory.bin := append-rootfs | pad-rootfs | pad-to 64k
+	IMAGE/sysupgrade.bin/squashfs := append-rootfs | pad-to 64k | sysupgrade-tar rootfs=$$$$@ | append-metadata
+	DEVICE_PACKAGES := ipq-wifi-zyxel_nbg7815 kmod-ath11k-pci e2fsprogs kmod-fs-ext4 losetup \
+	kmod-hwmon-tmp103 kmod-bluetooth
+endef
+TARGET_DEVICES += zyxel_nbg7815
+
